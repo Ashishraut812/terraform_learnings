b18160ac9157b6e7f8495b65f1d2c32ffda8ff47
Ashishraut812 <812ashishraut1999@gmail.com>
2024-12-12 12:34:08 +0530

    first terraform file - main.tf

---
+++ main.tf
@@ -0,0 +1,4 @@
+resource "local_file" "new_file" {
+	filename = "/Users/ashishvitthalraoraut/Desktop/study/terraform/terraform_learnings/one.txt"
+	content = "This is first file created using terraform!!"
+}
